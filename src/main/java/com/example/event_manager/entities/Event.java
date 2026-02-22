15bce210d61bc242bc7d5407dcc1fe3a6d2b4a42
Patricia <patriciastarck@gmail.com>
2026-02-22 19:44:01 -0300

    feat: implementar entidades core e repositórios de eventos e administradores

---
+++ src/main/java/com/example/event_manager/entities/Event.java
@@ -0,0 +1,32 @@
+package com.example.event_manager.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_event")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String location;
+
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "administrator_id", nullable = false)
+    private Administrator administrator;
+
+}
---
+++ src/main/java/com/example/event_manager/repositories/EventRepository.java
@@ -0,0 +1,11 @@
+package com.example.event_manager.repositories;
+
+import com.example.event_manager.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    // Busca todos os eventos que pertencem a um admin específico
+    List<Event> findByAdministratorId(Long adminId);
+}
