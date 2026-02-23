6b08b923281feb95e0e554224a12292c3fe6e55c
Patricia <patriciastarck@gmail.com>
2026-02-23 16:09:21 -0300

    feat: configurar autenticação JWT e documentação da API com Swagger

--- src/main/java/com/example/event_manager/config/SwaggerConfig.java
+++ src/main/java/com/example/event_manager/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.event_manager.configs;
+package com.example.event_manager.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
--- src/main/java/com/example/event_manager/security/TokenService.java
+++ src/main/java/com/example/event_manager/security/TokenService.java
@@ -3,7 +3,7 @@ package com.example.event_manager.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.example.event_manager.entities.Administrator;
+import com.example.event_manager.domain.entities.Administrator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
